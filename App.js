378fba8e6e5b861d456210d9dc96df106ff6df13
Ana María <avelazquez46@alumnos.uaq.mx>
2025-02-19 16:42:58 -0600

    Examen

--- App.js
+++ App.js
@@ -1,12 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
+import CheckBox from 'react-native-check-box';
+import { useState } from 'react';
 
 export default function App() {
+  
+const [check1, setCheck1] = useState(false);
+const [check2, setCheck2] = useState(false);
+const [check3, setCheck3] = useState(false);
+const [check4, setCheck4] = useState(false);
+  
+const checkedHandler1 = () => {
+  checked1 ? setChecked1(false): setChecked1(true);
+}
+
+const checkedHandler2 = () => {
+  checked2 ? setChecked2(false): setChecked2(true);
+}
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View> <Text style={{fontSize: 28}}>Compras</Text></View>
       <StatusBar style="auto" />
+
+      <View>
+        <View style={styles.productContainer}>
+          <Text>Escribe el producto que desees añadir</Text>
+          <TextInput style={{opacity:.5}}>producto</TextInput>
+        </View>
+      </View>
+
+      <ScrollView>
+      <View style={styles.checkboxcontainer}> 
+        <CheckBox title="leche" checked={check1} onPress={checkedHandler1}/>
+        <CheckBox title="leche" checked={check2} onPress={checkedHandler2}/>
+        <CheckBox title="leche" checked={check1} onPress={checkedHandler1}/>
+        <CheckBox title="leche" checked={check2} onPress={checkedHandler2}/>
+        <CheckBox title="leche" checked={check1} onPress={checkedHandler1}/>
+        <CheckBox title="leche" checked={check2} onPress={checkedHandler2}/>
+        <CheckBox title="leche" checked={check1} onPress={checkedHandler1}/>
+        <CheckBox title="leche" checked={check2} onPress={checkedHandler2}/>
+        <CheckBox title="leche" checked={check1} onPress={checkedHandler1}/>
+        <CheckBox title="leche" checked={check2} onPress={checkedHandler2}/>
+        <CheckBox title="leche" checked={check1} onPress={checkedHandler1}/>
+        <CheckBox title="leche" checked={check2} onPress={checkedHandler2}/>
+        <CheckBox title="leche" checked={check1} onPress={checkedHandler1}/>
+        <CheckBox title="leche" checked={check2} onPress={checkedHandler2}/>
+        <CheckBox title="leche" checked={check1} onPress={checkedHandler1}/>
+        <CheckBox title="leche" checked={check2} onPress={checkedHandler2}/>
+        <CheckBox title="leche" checked={check1} onPress={checkedHandler1}/>
+        <CheckBox title="leche" checked={check2} onPress={checkedHandler2}/>
+        <CheckBox title="leche" checked={check1} onPress={checkedHandler1}/>
+        <CheckBox title="leche" checked={check2} onPress={checkedHandler2}/>
+        <CheckBox title="leche" checked={check1} onPress={checkedHandler1}/>
+        <CheckBox title="leche" checked={check2} onPress={checkedHandler2}/>
+
+
+        
+      </View>
+      </ScrollView>
+      
+
     </View>
+   
+
+    
+
   );
 }
 
@@ -16,5 +76,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 120,
+    marginBottom: 100,
+    
   },
+  productContainer: {
+    backgroundColor: "#C28CAE",
+    width: 250,
+    height: 75,
+    marginTop: 50,
+    justifyContent: 'center',
+    paddingLeft: 10,
+    borderRadius: 15
+
+  },
+  checkboxcontainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 100 ,
+    paddingRight: 200,
+    fontSize:10
+  }
 });
